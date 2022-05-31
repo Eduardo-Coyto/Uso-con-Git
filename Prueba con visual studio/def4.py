089554118edceddcb28656888b91121dd29c3d75
Eduardo Coyto Brignone <79756539+Eduardo-Coyto@users.noreply.github.com>
2022-05-31 12:58:14 -0300

    Update and rename tarea7.py to def4.py
    cambio de nombre de archivo y agregue comentarios al código

--- Prueba con visual studio/def4.py
+++ Prueba con visual studio/def4.py
@@ -2,9 +2,9 @@ nombres=[]
 edades=[]
 for x in range(5):
     nom=input("Ingrese el nombre de la persona:")
-    nombres.append(nom)
+    nombres.append(nom) # lo agrego a la lista nombres
     ed=int(input("Ingrese la edad de dicha persona:"))
-    edades.append(ed)
+    edades.append(ed) # lo agrego a la lista de edades
 
 print("Nombre de las personas mayores de edad:")
 for x in range(5):
